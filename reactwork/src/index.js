f50154e017be4572bbe6ad1b5cfc5d614ca48af3
sihyeon2 <tlugs1426@naver.com>
2023-10-24 11:33:01 +0900

    20231024_1132

---
+++ reactwork/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+import App from './App';
+// .js와 .jsx는 같은 의미이기 같은 폴더에 있으면 확장자 생략 가능
+// import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
